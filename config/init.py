7ca467fbfe07ebbbdfb6d25f0b621089b6c96cb2
cgartco6 <155203611+cgartco6@users.noreply.github.com>
2025-12-13 22:48:52 +0200

    Add configuration module for Soccer Prediction System

---
+++ config/init.py
@@ -0,0 +1,6 @@
+"""
+Configuration module for Soccer Prediction System
+"""
+from .settings import Config, ScraperConfig, DatabaseConfig, ModelConfig, APIConfig
+
+__all__ = ['Config', 'ScraperConfig', 'DatabaseConfig', 'ModelConfig', 'APIConfig']
